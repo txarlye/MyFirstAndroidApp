cbe0940aab6762d72c076dfb55abdda83a462c35
Txarlye <txarlye@gmaill.com>
2024-03-02 13:45:55 +0100

    ultimo reto layouts!

---
+++ app/src/main/java/com/example/myapplication_01/FirstApp/EjercicioLAyout03ScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication_01.FirstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication_01.R
+
+class EjercicioLAyout03ScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio_layout03_screen)
+    }
+}
