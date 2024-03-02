924471d8db2c679d66df2ac1b72661ccf3bf393f
Txarlye <txarlye@gmaill.com>
2024-03-02 09:59:26 +0100

    primer reto layouts

---
+++ app/src/main/java/com/example/myapplication_01/FirstApp/EjercicioLayout01ScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication_01.FirstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication_01.R
+
+class EjercicioLayout01ScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejercicio_layout01_screen)
+    }
+}
