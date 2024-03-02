970868ab68e80e4538bbac9ff6c92f13dae4f596
Txarlye <txarlye@gmaill.com>
2024-03-02 20:36:20 +0100

    4h- segunda pantalla

--- app/src/main/java/com/example/myapplication_01/FirstApp/FirstAppActivity.kt
+++ app/src/main/java/com/example/myapplication_01/FirstApp/FirstAppActivity.kt
@@ -2,12 +2,24 @@ package com.example.myapplication_01.FirstApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.example.myapplication_01.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnClickStart)
+        val etName = findViewById<AppCompatEditText>(R.id.editTextUser)
+
+
+        btnStart.setOnClickListener{
+            Log.i("CursoAristidevs", "Button pulsado ${etName.text.toString()}")
+        }
 
 
     }
---
+++ app/src/main/java/com/example/myapplication_01/FirstApp/ResultMainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication_01.FirstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.myapplication_01.R
+
+class ResultMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_main)
+        val tvResult = findViewById<TextView>(R.id.textViewResult)
+
+    }
+}
